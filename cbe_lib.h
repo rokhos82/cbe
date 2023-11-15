1061bfb2e0ea60b4bd2b2add19f7550988f716de
Justin Lane <justin.l.lane@gmail.com>
2023-11-15 12:29:34 -0700

    eol char fix

--- cbe_lib.h
+++ cbe_lib.h
@@ -8,3 +8,5 @@ BE::FleetInfo parseFleetHeader(string header);
 void debugPrintUnits();
 string attShipStr(int i);
 string defShipStr(int i);
+
+std::string fixEOLCharacters(const std::string &original);
