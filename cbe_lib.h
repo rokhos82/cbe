f42fed370c403b5c138f0b2296313aae3995debe
Justin Lane <justin.l.lane@gmail.com>
2023-11-28 15:02:08 -0700

    actions

--- cbe_lib.h
+++ cbe_lib.h
@@ -10,3 +10,10 @@ string attShipStr(int i);
 string defShipStr(int i);
 
 std::string fixEOLCharacters(const std::string &original);
+
+namespace BE {
+        enum class actions {
+        ATTACK,
+        RETREAT
+    };
+};
