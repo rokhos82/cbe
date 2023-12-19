f5309d2164338134ad79dd69ee9f0a912d9fc0ed
Justin Lane <justin.l.lane@gmail.com>
2023-12-19 11:31:50 -0700

    Formatting

--- engine.cpp
+++ engine.cpp
@@ -5,57 +5,73 @@
 
 namespace BE
 {
-    engine::engine() {};
-    void engine::loadFleets(const std::string_view & fileName) {}
-    void engine::loadAttackingFleet(const std::string_view & fileName) {}
-    void engine::loadDefendingFleet(const std::string_view & fileName) {}
+    engine::engine(){};
+    void engine::loadFleets(const std::string_view &fileName) {}
+    void engine::loadAttackingFleet(const std::string_view &fileName) {}
+    void engine::loadDefendingFleet(const std::string_view &fileName) {}
 
-    void engine::start(const std::vector<std::string_view> & args) {
+    void engine::start(const std::vector<std::string_view> &args)
+    {
         this->parseArguments(args);
     }
     void engine::printUI() {}
     void engine::printCLI() {}
-    void engine::parseArguments(const std::vector<std::string_view> & args) {
-        for(auto cmd = args.begin(), end = args.end(); cmd != end;cmd++) {
-            if(*cmd == "-a" || *cmd == "--attackers") {
+    void engine::parseArguments(const std::vector<std::string_view> &args)
+    {
+        for (auto cmd = args.begin(), end = args.end(); cmd != end; cmd++)
+        {
+            if (*cmd == "-a" || *cmd == "--attackers")
+            {
                 cmd++;
-                if(cmd != end) {
+                if (cmd != end)
+                {
                     this->loadAttackingFleet(*cmd);
                 }
-                else {
+                else
+                {
                     std::cerr << "No attackers file specified with [-a|--attackers]." << std::endl;
                 }
             }
-            else if(*cmd == "-d" || *cmd == "--defenders") {
+            else if (*cmd == "-d" || *cmd == "--defenders")
+            {
                 cmd++;
-                if(cmd != end) {
+                if (cmd != end)
+                {
                     this->loadDefendingFleet(*cmd);
                 }
-                else {
+                else
+                {
                     std::cerr << "No defenders file specified with [-d|--defenders]." << std::endl;
                 }
             }
-            else if(*cmd == "-e" || *cmd == "--fight") {
+            else if (*cmd == "-e" || *cmd == "--fight")
+            {
                 // Set headless here
                 this->headless = true;
             }
-            else if(*cmd == "-h" || *cmd == "--hit") {
+            else if (*cmd == "-h" || *cmd == "--hit")
+            {
                 cmd++;
-                if(cmd != end) {
+                if (cmd != end)
+                {
                     // Set baseToHit here
                 }
-                else {
+                else
+                {
                     std::cerr << "No number specified with [-h|--hit]." << std::endl;
                 }
             }
-            else if(*cmd == "-1" || *cmd == "--onestep") {
+            else if (*cmd == "-1" || *cmd == "--onestep")
+            {
                 // Set one-step here
                 this->oneStep;
             }
-            else if(*cmd == "-n" || *cmd == "--name") {
+            else if (*cmd == "-n" || *cmd == "--name")
+            {
                 // Set simulation name here
             }
-            else {
+            else
+            {
                 std::cerr << "Unknown command: " << *cmd << std::endl;
             }
         }
--- engine.h
+++ engine.h
@@ -1,24 +1,27 @@
 #pragma once
 
 #include <string>
-#include <vector> 
+#include <vector>
 
-namespace BE {
-    class engine {
-        public:
-            engine();
-            engine(const std::vector<std::string> args);
-            void loadFleets(const std::string_view & fileName);
-            void start(const std::vector<std::string_view> & args);
-        private:
-            bool headless;
-            bool oneStep;
-            
-            void loadAttackingFleet(const std::string_view & fileName);
-            void loadDefendingFleet(const std::string_view & fileName);
+namespace BE
+{
+    class engine
+    {
+    public:
+        engine();
+        engine(const std::vector<std::string> args);
+        void loadFleets(const std::string_view &fileName);
+        void start(const std::vector<std::string_view> &args);
 
-            void printUI();
-            void printCLI();
-            void parseArguments(const std::vector<std::string_view> & args);
+    private:
+        bool headless;
+        bool oneStep;
+
+        void loadAttackingFleet(const std::string_view &fileName);
+        void loadDefendingFleet(const std::string_view &fileName);
+
+        void printUI();
+        void printCLI();
+        void parseArguments(const std::vector<std::string_view> &args);
     };
 }
